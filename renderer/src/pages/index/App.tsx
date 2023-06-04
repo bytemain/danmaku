0025954b353e3721d2947640cdbd4b346656607c
artin <artin@cat.ms>
2023-06-04 12:35:24 +0800

    chore: remove unused code

--- renderer/src/pages/index/App.tsx
+++ renderer/src/pages/index/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from '@/assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { useLocalStorageState } from 'ahooks';
 
@@ -10,14 +8,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
       <h1>请输入房间号</h1>
       <input
         value={roomId}
@@ -38,9 +28,6 @@ function App() {
           Open Danmaku Page
         </button>
       </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
