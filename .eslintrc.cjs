563fe43a46921e860e242c9b7cf886037f03c782
artin <artin@cat.ms>
2023-06-04 13:07:16 +0800

    chore: update eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,18 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  rules: {
+    'no-unused-vars': 'off',
+    'no-undef': 'off',
+  },
 };
