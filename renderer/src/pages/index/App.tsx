806fdd73b6c9215da640b9efdfe34282c507d550
artin <artin@cat.ms>
2023-06-16 00:04:48 +0800

    feat: support room id history

--- renderer/src/pages/index/App.tsx
+++ renderer/src/pages/index/App.tsx
@@ -1,10 +1,36 @@
 import './App.css';
 import { useLocalStorageState } from 'ahooks';
+import { Box } from '@chakra-ui/react';
+import React from 'react';
 
 function App() {
   const [roomId, setRoomId] = useLocalStorageState('roomId', {
     defaultValue: '',
   });
+  const [history, setHistory] = useLocalStorageState<string[]>(
+    'roomIdHistory',
+    {
+      defaultValue: [] as string[],
+    }
+  );
+
+  const renderHistory = React.useCallback(() => {
+    if (!history || history.length === 0) {
+      return null;
+    }
+    return (
+      <Box>
+        <Box>History:</Box>
+        {history.map((item) => {
+          return (
+            <Box as='span' cursor={'pointer'} onClick={() => setRoomId(item)}>
+              {item}
+            </Box>
+          );
+        })}
+      </Box>
+    );
+  }, [history]);
 
   return (
     <>
@@ -15,6 +41,7 @@ function App() {
           setRoomId(event.target.value);
         }}
       ></input>
+      {renderHistory()}
       <div className='card'>
         <button
           onClick={() => {
@@ -22,6 +49,8 @@ function App() {
               alert('请输入房间号');
               return;
             }
+
+            setHistory([roomId, ...(history ?? [])]);
             danmaku.open(roomId);
           }}
         >
